7eba335784eff276f094f9293dd68d0106422296
Sofi Morales <137367136+SofiMorales99@users.noreply.github.com>
2023-07-14 21:43:21 -0300

    Add files via upload

---
+++ java/javaFun/MiPrimerPrograma.java
@@ -0,0 +1,12 @@
+public class MiPrimerPrograma {
+    
+    public static void main(String[] args) {
+        String nombre = "Sofia";
+        int edad = 23;
+        String ciudad = "Adrogue";
+
+        System.out.println("Mi nombre es "+ nombre);
+        System.out.println("Tengo "+ edad +" anios");
+        System.out.println("Vivo en "+ ciudad);
+    }
+}
